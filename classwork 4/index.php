71858ea78d6226504f9e060d68d6c6f49c92092a
roshaanbutt <44616248+roshaanbutt@users.noreply.github.com>
2019-02-05 14:33:54 +0500

    Add files via upload

---
+++ classwork 4/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1> HTML Forms </h1>
+<form action"" method="post">
+<table>
+	<tr>
+		<td> name </td>
+		<td>
+			<input type="text" value= "">
+		</td>
+	</tr>	
+		
+	<tr>	
+		<td> email</td>
+		<td>
+			<input type="email" value "">
+		</td>
+	</tr>
+
+	<tr>	
+		<td> password</td>
+		<td>
+			<input type="password" value "password">
+		</td>
+	</tr>	
+		
+	<tr>	
+		<td>gender</td>
+		<td>
+		<select>
+			<option value = "m"> male </option>
+			<option value="f">female </option>
+		</select>	
+		</td>
+	</tr>
+
+	<tr>
+		<td> gender </td>
+			<td>
+				<input name="user_gender" type="radio" value="male">
+				<input name="user_gender" type="radio" value="female">
+			</td>
+	</tr>
+	
+	
+	<tr>
+		<td>
+			<input type="submit" value "submit">
+		</td>
+		
+	</tr>
+
+</table>
